c234a8ea08cd8ea9a0906418893de617ee8d33e2
David Kiss <davidkissdev@gmail.com>
2022-11-08 10:16:43 +0100

    add more setup

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,9 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
