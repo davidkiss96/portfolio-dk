2198b74e32c4a76f22726e25db1157498ce9af05
David Kiss <davidkissdev@gmail.com>
2023-03-26 14:06:35 +0200

    prebuild

---
+++ public/studio/static/index-c20e30e3-341e3060.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-63f13ccc-81c95bf9.js";import"./sanity-aa0033ef.js";import"./index-45efd282.js";export{m as DocumentPaneProvider,a as default};
